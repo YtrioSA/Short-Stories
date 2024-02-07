43b211f332e1aca0a6f916d6eabcd03ac594182f
Ytrio <36244118+YtrioSA@users.noreply.github.com>
2024-02-07 00:49:49 -0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,12 +18,12 @@
 
 <Toast />
 
-<div class="bg-[#000] font-bold flex flex-col gap-5 justify-center items-center h-full w-full">
+<div class="bg-[#000] font-bold flex justify-center items-center h-full w-full">
     <audio autoplay loop>
         <source src="./The-Home/rain_thunder.mp3" type="audio/mp3">
         Seu navegador não suporta o elemento de áudio.
     </audio>
-    <p class="cursor-pointer w-1/5">
+    <p class="w-1/2 cursor-pointer md:w-1/4 lg:w-1/2 xl:w-1/4 ">
         <q>
             Sad people love the rain
             because they are no longer
